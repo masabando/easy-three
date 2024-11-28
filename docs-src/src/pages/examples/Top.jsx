43ac529be070a5339f1c6b6c5ca47189494833f1
Masamitsu Bando <bando@ktc.ac.jp>
2024-11-28 19:57:18 +0900

    update

--- docs-src/src/pages/examples/Top.jsx
+++ docs-src/src/pages/examples/Top.jsx
@@ -5,6 +5,8 @@ export default function Examples() {
     <Container className="pt-4 pb-5">
       <title>Examples | easy-three</title>
       <h1>Examples</h1>
+
+      まだないよ！
     </Container>
   );
 }
