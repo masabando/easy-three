177d6fb44930a2b68418c57c6019fa46bdd61a44
Masamitsu Bando <bando@ktc.ac.jp>
2024-12-03 12:14:36 +0900

    update

---
+++ src/components/CodeBlock.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { ConfigProvider, Flex, Popover } from "antd";
+import { BsCopy } from "react-icons/bs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import T from "./Lang";
+
+export default function CodeBlock({
+  language = "javascript",
+  filename,
+  className = "",
+  style = {},
+  showLineNumbers = false,
+  children,
+}) {
+  return (
+    <div className={`my-3 ${className}`} style={style}>
+      {filename && (
+        <Flex
+          justify="space-between"
+          className="bg-secondary text-white px-3 py-2 rounded-top"
+          style={{
+            fontSize: "90%",
+          }}
+        >
+          {filename}
+          <ConfigProvider
+            theme={{
+              components: {
+                Popover: {
+                  titleMinWidth: 10,
+                },
+              },
+            }}
+          >
+            <Popover
+              content={
+                <div>
+                  <T>
+                    <>copied to clipboard!</>
+                    <>クリップボードにコピーしました！</>
+                  </T>
+                </div>
+              }
+              title=""
+              trigger="click"
+            >
+              <div>
+                <BsCopy
+                  className="ms-auto"
+                  onClick={() => {
+                    navigator.clipboard.writeText(children);
+                  }}
+                />
+              </div>
+            </Popover>
+          </ConfigProvider>
+        </Flex>
+      )}
+      <SyntaxHighlighter
+        showLineNumbers={showLineNumbers}
+        language={language}
+        style={a11yDark}
+        className={`${filename ? "rounded-bottom" : "rounded"} p-3`}
+      >
+        {children}
+      </SyntaxHighlighter>
+    </div>
+  );
+}
---
+++ src/components/Lang.jsx
@@ -0,0 +1,66 @@
+"use client";
+import { ConfigProvider, Segmented } from "antd";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const initialConfig = {
+  lang: "en",
+  setLang: () => {},
+};
+
+// eslint-disable-next-line
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [config, setConfig] = useState(initialConfig);
+  return (
+    <UserContext.Provider
+      value={{
+        config,
+        setConfig,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function LangSwitcher() {
+  const { config, setConfig } = useContext(UserContext);
+  useEffect(() => {
+    const storedConfig = localStorage.getItem("config");
+    if (storedConfig) {
+      setConfig(JSON.parse(storedConfig));
+    }
+    // eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("config", JSON.stringify(config));
+  }, [config]);
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Segmented: {
+            itemSelectedBg: "#1890ff",
+          },
+        },
+      }}
+    >
+      <Segmented
+        className="mx-1"
+        size="small"
+        options={[
+          { label: "Eng", value: "en" },
+          { label: "日本語", value: "ja" },
+        ]}
+        value={config.lang}
+        onChange={(value) => setConfig({ ...config, lang: value })}
+      />
+    </ConfigProvider>
+  );
+}
+
+export default function T({ children }) {
+  const { config } = useContext(UserContext);
+  return <>{config.lang === "en" ? children[0] : children[1] ?? children[0]}</>;
+}
