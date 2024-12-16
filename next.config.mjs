5d4413e807346562365dedb40652247a16a4d31c
Masamitsu Bando <bando@ktc.ac.jp>
2024-12-17 08:44:36 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0",
-          }
-        ]
-      }
-    ]
-  },
   output: "export",
   //distDir: "docs",
   distDir: "out",
