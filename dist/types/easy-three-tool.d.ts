e8dba0e62904a8866ee4346420b2f221083f8ada
Masamitsu Bando <bando@ktc.ac.jp>
2026-07-23 13:01:55 +0900

    feat: TransformControlsとCSG機能を追加
    EasyThreeTransformControlsインターフェースを追加し、オブジェクトをアタッチする機能を実装しました。また、EasyThreeToolインターフェースにCSG演算機能を追加し、2つのメッシュ間での演算を可能にしました。

--- dist/types/easy-three-tool.d.ts
+++ dist/types/easy-three-tool.d.ts
@@ -22,5 +22,35 @@ declare module "@masabando/easy-three" {
      * ```
      */
     distance(mesh1: THREE.Object3D, mesh2: THREE.Object3D, usePixelRatio?: boolean): number;
+
+    /**
+     * CSG 演算を行う。
+     * @param mesh1 CSG 演算対象のメッシュ1
+     * @param mesh2 CSG 演算対象のメッシュ2
+     * @param options CSG 演算オプション
+     * @returns CSG 演算後のメッシュ
+     * 
+     * ```js
+     * const cube = create.cube({ size: 1 })
+     * const sphere = create.sphere({ size: 0.7 })
+     *
+     * tool.csg(cube, sphere, {
+     *   remove: true, // 元のオブジェクトをシーンから削除するかどうか (デフォルト: true)
+     *   dispose: true, // 元のオブジェクトのマテリアルを破棄するかどうか (デフォルト: true)
+     * })
+     * ```
+     */
+    csg(mesh1: THREE.Mesh, mesh2: THREE.Mesh, options?: {
+      /**
+       * CSG 演算後に、元のオブジェクトをシーンから削除するかどうか。
+       * デフォルトは true で、削除されます。
+       */
+      remove?: boolean;
+      /**
+       * CSG 演算後に、元のオブジェクトのマテリアルを破棄するかどうか。
+       * デフォルトは true で、破棄されます。
+       */
+      dispose?: boolean;
+    }): void;
   }
 }
