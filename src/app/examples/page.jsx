3c8e98b8acec446adf87a4b169f6b6580c4d8b7b
Masamitsu Bando <bando@ktc.ac.jp>
2026-07-24 14:55:39 +0900

    サンプルを追加

--- src/app/examples/page.jsx
+++ src/app/examples/page.jsx
@@ -8,23 +8,22 @@ export const metadata = {
 
 function ExampleLink({ name, dir, creator }) {
   return (
-    <div className="w-40 shadow-lg bg-gray-800">
+    <div className="w-40 shadow-lg bg-gray-800 rounded-box overflow-hidden">
       <a
         href={`/easy-three/page/examples/${dir}/index.html`}
         className="block w-full h-40 overflow-hidden"
-      >
-        <img
-          src={`/easy-three/page/examples/${dir}/display.jpg`}
-          alt={name}
-          className="object-cover"
-        />
-      </a>
+        style={{
+          backgroundImage: `url(/easy-three/page/examples/${dir}/display.jpg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></a>
       <div className="bg-gray-800 text-white p-2 text-sm">
-        <div className="text-lg">{name}</div>
-        {creator && <div>by {creator}</div>}
+        <div className="text-base">{name}</div>
+        {creator && <div className="text-small">by {creator}</div>}
         <div className="mt-2">
           <a
-            className="btn btn-primary btn-sm btn-soft"
+            className="btn btn-primary btn-xs btn-soft"
             href={`https://github.com/masabando/easy-three/blob/dev/public/page/examples/${dir}/index.html`}
           >
             View Code
@@ -40,7 +39,9 @@ export default function Page() {
     <Container>
       <H1>Examples</H1>
 
-      <div className="flex justify-start flex-wrap gap-4 mt-5">
+      <div className="flex justify-around flex-wrap gap-4 mt-5">
+        <ExampleLink dir="pulsefield" name="Pulsefield" creator="masabando" />
+        <ExampleLink dir="nekoTower" name="Neko Tower" creator="masabando" />
         <ExampleLink dir="kinetictypography" name="Kinetic Typography" creator="Codex" />
         <ExampleLink dir="tinycity" name="Tiny City" creator="Codex" />
         <ExampleLink dir="floatinglanterns" name="Floating Lanterns" creator="Codex" />
@@ -48,7 +49,6 @@ export default function Page() {
         <ExampleLink dir="weathercubes" name="Weather Cubes" creator="Codex" />
         <ExampleLink dir="moonjelly" name="Moon Jelly" creator="Codex" />
         <ExampleLink dir="signalgarden" name="Signal Garden" creator="Codex" />
-        <ExampleLink dir="pulsefield" name="Pulsefield" creator="masabando" />
         <ExampleLink dir="dice" name="Dice" creator="KUTC-KaedeYuto" />
         <ExampleLink dir="events" name="Events" creator="KUTC-KaedeYuto" />
       </div>
